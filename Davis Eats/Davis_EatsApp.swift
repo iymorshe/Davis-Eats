f983ddf978b38a6dbedb83500aa6248dce6c0f81
iymorshe <92063753+iymorshe@users.noreply.github.com>
2024-04-29 17:47:14 -0700

    Initial Commit

---
+++ Davis Eats/Davis_EatsApp.swift
@@ -0,0 +1,20 @@
+//
+//  Davis_EatsApp.swift
+//  Davis Eats
+//
+//  Created by Iman Morshed on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Davis_EatsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
